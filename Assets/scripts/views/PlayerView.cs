7cf7959ec8dd260ea1860c73122b90b4cd7f41a5
Ziv <zivshaul3@gmail.com>
2023-07-08 01:34:03 +0300

    Created player movement and other player inputs

--- Assets/scripts/views/PlayerView.cs
+++ Assets/scripts/views/PlayerView.cs
@@ -2,17 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerView : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class PlayerView: MonoBehaviour {
+	[SerializeField] private Rigidbody2D playerRigidBody;
+	[SerializeField] private SpriteRenderer spriteRenderer;
+	//[SerializeField] private Animator animator;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private enum MovementState {
+		idle, running//, jumping, falling, doubleJump
+	}
+
+	private void UpdateAnimation() {
+		MovementState movementState = MovementState.idle;
+
+		if (playerRigidBody.velocity.x > 0) {
+			spriteRenderer.flipX = false;
+			movementState = MovementState.running;
+		} else if (playerRigidBody.velocity.x < 0) {
+			spriteRenderer.flipX = true;
+			movementState = MovementState.running;
+		} else {
+			movementState = MovementState.idle;
+		}
+
+		//if (playerRigidBody.velocity.y > .1f && !CanDoubleJump) {
+		//	movementState = MovementState.doubleJump;
+		//} else if (playerRigidBody.velocity.y > .1f && CanDoubleJump) {
+		//	movementState = MovementState.jumping;
+		//} else if (playerRigidBody.velocity.y < -.1f) {
+		//	movementState = MovementState.falling;
+		//}
+
+		//animator.SetInteger("movement", (int)movementState);
+
+	}
 }
